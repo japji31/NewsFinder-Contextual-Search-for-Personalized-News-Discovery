e33324612adad6bc86933bf3b711235ec9cf8d9d
Japji <yps.japji@gmail.com>
2024-04-05 01:39:29 +0530

    added django signup and login fucntionality

--- django_service/news_search_django/urls.py
+++ django_service/news_search_django/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from search_app import views
 
 urlpatterns = [
-    path('news_search/', views.context_search,name="context_search"),
     path('admin/', admin.site.urls),
+    path('', views.user_signup, name="signup"),
+    path('login/', views.user_login, name="login"),
+    path('news/', views.user_ml, name="news"),
+    path('news_search/', views.context_search, name="context_search"),
 ]
--- django_service/search_app/views.py
+++ django_service/search_app/views.py
@@ -1,14 +1,79 @@
 from django.http import JsonResponse
-from django.views.decorators.http import require_POST   
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
 from .searching import SearchNews
 from .models import SearchResult
 from dotenv import load_dotenv
 import os
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.decorators import login_required
+
+# @login_required(login_url='login')
+
+def user_signup(request):
+
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+
+        if pass1!=pass2:
+            return HttpResponse("Your password and confrom password are not Same!!")
+        else:
+            my_user=User.objects.create_user(uname,email,pass1)
+            my_user.save()
+            return redirect('login')
+
+    return render (request,'signup.html')
+
+     
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        user=authenticate(request,username=username,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('news')
+        else:
+            return HttpResponse ("Username or Password is incorrect!!!")
+
+    return render (request,'login.html')
+
+def user_ml(request):
+    if request.method == 'POST':
+        user_query = request.POST.get('user_query')
+        load_dotenv()
+        connection_string = os.getenv('connection_string')
+        search_news = SearchNews(connection_string)
+    
+        # Generate embedding for user query
+        user_embedding = search_news.generate_embedding(user_query)
+    
+        # Retrieve top similar news items
+        top_news = search_news.retrieve_top_news(user_embedding)
+    
+        # Prepare the response data
+        response_data = {
+            "user_query": user_query,
+            "similar_news": top_news
+        }
+    
+        search_result = SearchResult.objects.create(
+            user_query=user_query,
+            response_list=top_news  
+        )
+    
+        # Return the response as JSON
+        return JsonResponse(response_data)
+
+    else:
+        return render(request, 'news.html')
 @csrf_exempt
-@require_POST
 def context_search(request):
-    # Extract user query from the POST request data
     user_query = request.POST.get('user_query')
     load_dotenv()
     connection_string = os.getenv('connection_string')
@@ -31,5 +96,4 @@ def context_search(request):
         response_list=top_news  
     )
 
-    # Return the response as JSON
     return JsonResponse(response_data)
